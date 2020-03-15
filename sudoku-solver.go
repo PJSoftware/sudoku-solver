679cf36aca7200272a8f38027d8dab988f0270c7
Peter Jones <pjsoftware@petesplace.id.au>
2020-03-15 14:50:25 +1000

    let's try importing our test file

--- sudoku-solver.go
+++ sudoku-solver.go
@@ -1,5 +1,7 @@
 package main
 
+import "./sudoku"
+
 // 1. Initialise grid
 // 2. Solve grid
 // 3. Display solution
@@ -7,5 +9,6 @@ package main
 const defaultPuzzleFile string = "puzzles/easy.sp"
 
 func main() {
-
+	grid := sudoku.NewGrid()
+	grid.Import(defaultPuzzleFile)
 }
