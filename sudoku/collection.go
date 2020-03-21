725e7f0d188662570b4a31247e6105983397af69
Peter Jones <pjsoftware@petesplace.id.au>
2020-03-22 09:19:49 +1000

    third solver 'ExtendPossVal' added

--- sudoku/collection.go
+++ sudoku/collection.go
@@ -38,3 +38,43 @@ func displayColl(cName string, cArray [gridSize]collection) {
 		fmt.Println()
 	}
 }
+
+func (cc collection) emptyCount() int {
+	ec := 0
+	for i := range gridCoord {
+		if cc[i].val == empty {
+			ec++
+		}
+	}
+	return ec
+}
+
+func (cc collection) usedValues() []value {
+	used := cc.usedMap()
+	var rv []value
+	for val := range used {
+		rv = append(rv, val)
+	}
+	return rv
+}
+
+func (cc collection) unusedValues() []value {
+	used := cc.usedMap()
+	var unused []value
+	for _, val := range values {
+		if _, ok := used[val]; !ok {
+			unused = append(unused, val)
+		}
+	}
+	return unused
+}
+
+func (cc collection) usedMap() map[value]bool {
+	used := make(map[value]bool)
+	for i := range gridCoord {
+		if cc[i].val != empty {
+			used[cc[i].val] = true
+		}
+	}
+	return used
+}
--- sudoku/gridsolve.go
+++ sudoku/gridsolve.go
@@ -13,6 +13,7 @@ type solver struct {
 var solvers = []solver{
 	solver{"OnlyPossibleValue", (*Grid).solveUseOPV},
 	solver{"OPVByBlock", (*Grid).solveOPVbyBlock},
+	solver{"ExtendPossVal", (*Grid).solveExtendPossVal},
 }
 
 // Solve the grid
@@ -39,7 +40,6 @@ func (g *Grid) Solve() {
 		if g.showWorking {
 			g.Display()
 		}
-		fmt.Printf("Pass %d: %d cells solved\n", pass, numSolved)
 		if numSolved == 0 {
 			fmt.Printf("Solver is stuck with %d empty cells remaining\n", g.emptyCells())
 			return
@@ -92,4 +92,74 @@ func (g *Grid) solveOPVbyBlock() (int, error) {
 		}
 	}
 	return nowEmpty - g.emptyCells(), nil
+}
+
+func (g *Grid) solveExtendPossVal() (int, error) {
+	ext := 0
+	for bi := range gridCoord {
+		ec := blkColl[bi].emptyCount()
+		if ec >= 2 && ec <= 3 {
+			rows := make(map[int]bool)
+			cols := make(map[int]bool)
+			for i := range gridCoord {
+				c := blkColl[bi][i]
+				if c.val == empty {
+					rows[c.ri] = true
+					cols[c.ci] = true
+				}
+			}
+			if len(rows) == 1 || len(cols) == 1 {
+				unused := blkColl[bi].unusedValues()
+
+				var cc collection
+				var fixed int
+				ignore := make(map[int]bool)
+
+				if len(rows) == 1 {
+					for i := range rows {
+						fixed = i
+					}
+					cc = rowColl[fixed]
+					for i := range cols {
+						ignore[i] = true
+					}
+				} else { // len(cols) == 1
+					for i := range cols {
+						fixed = i
+					}
+					cc = colColl[fixed]
+					for i := range rows {
+						ignore[i] = true
+					}
+				}
+
+				for _, val := range unused {
+					for i := range gridCoord {
+						c := cc[i]
+						skip := false
+						if len(rows) == 1 {
+							if _, ok := ignore[c.ci]; ok {
+								skip = true
+							}
+						} else {
+							if _, ok := ignore[c.ri]; ok {
+								skip = true
+							}
+						}
+						if skip {
+							continue
+						}
+
+						vi := int(val) - 1
+						if c.val == empty && c.possible[vi] {
+							g.working(fmt.Sprintf("Empty Cell(%d,%d), value %s set to not possible", c.ri, c.ci, val))
+							c.possible[vi] = false
+							ext++
+						}
+					}
+				}
+			}
+		}
+	}
+	return ext, nil
 }
